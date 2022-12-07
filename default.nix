718723a61f53d5c5d61283a834c8d5ba5e5aa178
Vasily Pashkov <pashkov@lvk.cs.msu.su>
2022-12-07 11:16:29 +0300

    Update default.nix
    update default.nix (to qt 5.15)

--- default.nix
+++ default.nix
@@ -54,7 +54,7 @@ in rec {
       libtins
       range-v3
       tiny-process
-      pkgs.libsForQt514.qtbase
+      pkgs.libsForQt515.qtbase
       pkgs.jq
     ]
       # Qt build is broken on OS X. Use system Qt instead.
